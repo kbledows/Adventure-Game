4cbf32b64c9ef2039125dca0a6701ccc8f38ecd6
Konrad <32736495+kbledows@users.noreply.github.com>
2021-12-29 17:53:52 -0500

    Added Title Screen
    Added title screen, reworked menu. WIP

--- Game.py
+++ Game.py
@@ -1,17 +1,64 @@
+#Python Text RPG
+#Konrad Bledowski 12-29-2021
 from Player import Character
 from Item import Item
+
 import time
+import cmd
+import textwrap
+import sys
+import os
+import random
+
+screen_width = 100
+classes = ["wizard","archer","knight"]
+options = ['play','help','quit']
+
+def title_screen_selections():
+    option = input("> ")
+    if option.lower() == ("play"):
+        start_game() #placeholder
+    elif option.lower() == ("help"):
+        help_menu()
+    elif option.lower() == ("quit"):
+        sys.exit()
+    while option.lower() not in options:
+        print("Invalid Option!")
+        print("Choose from " + str(options))
+        title_screen_selections()
+
+def title_screen():
+    os.system('clear')
+    print("###################################")
+    print("# Welcome to Realm of the Mad God #")
+    print("###################################")
+    print("             -- Play --            ")
+    print("             -- Help --            ")
+    print("             -- Quit --            ")
+    print("")
+    title_screen_selections()
+
+
+def help_menu():
+    pass
+
+def start_game():
+    print("Please enter a name")
+    name = input("> ")
+    while True:
+        print("Please choose a player class!")
+        print("Class options: " + str(classes))
+        player_class = input("> ").lower()
+        if player_class not in classes:
+            print("Invalid Option")
+        else:
+            break
+
+    p = Character(name,player_class)
+
 
 def main():
-    print("Hello World!")
-    print("Please enter a name")
-    name = input()
-    print("Please enter a sex")
-    sex = input()
-
-    p = Character(name,sex)
-    print(p.name)
-    print(p.sex)
+    title_screen()
 
 if __name__ == "__main__":
 	main()
--- Player.py
+++ Player.py
@@ -1,11 +1,14 @@
 class Character:
-    def __init__(self, name, sex):
+    def __init__(self, name, char_class):
         self.name = name
-        self.sex = sex
+        self.char_class = char_class
         self.inventory = {
             "gold": 100,
             "weapons": []
         }
+        self.hp = 0
+        self.mp = 0
+        self.status_effects = []
         self.coordinates = { #use coord plane for map
             'xCoord': 0,
             'yCoord': 0,
@@ -13,6 +16,16 @@ class Character:
     
     def get_inventory(self):
         return self.inventory
+    
+    def get_hp(self):
+        return self.hp
+    def set_hp(self,hp):
+        self.hp = hp
+    def get_mp(self):
+        return self.mp
+    def set_mp(self,mp):
+        self.mp = mp
+    #implement heal hp and heal mp - cannot overflow maxHP
 
     def get_coordinates(self):
         return self.coordinates
@@ -23,10 +36,9 @@ class Character:
     def set_coordinates(self,x,y):
         self.coordinates['xCoord'] = x
         self.coordinates['yCoord'] = y
-        
-    def get_sex(self):
-        return self.sex
     
+
+#Notes: Perhaps make a general class that is "Mob" with HP stat... coudl be useful.
 
 
 
